f526b8615c10e32c30b6121bb142351e7558ebfb
Tamer <tmansour720@gmail.com>
2022-01-13 13:42:14 +0200

    Validate-in-function-section

--- learn-react-projects/functional-components-section/src/App.js
+++ learn-react-projects/functional-components-section/src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
 import './App.css';
+import FormInput from './Components/FormInput';
+import React, { useState } from 'react';
+const App = () => {
+  const [values, setValues] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+  });
 
-function App() {
+  const initialState = [
+    {
+      id: 1, name: "firstName",
+      type: "text",
+      placeholder: "First Name",
+      errorMessage:
+        "your first name requierd",
+      label: "First Name",
+
+      required: true,
+    }, {
+      id: 2, name: "lastName",
+      type: "text",
+      placeholder: "Last Name",
+      errorMessage:
+        "your last name requierd",
+      label: "Last Name",
+
+      required: true,
+    },
+
+    {
+      id: 3,
+      name: "email",
+      type: "email",
+      placeholder: "Email",
+      errorMessage: "It should be a valid email address!",
+      label: "Email",
+      required: true,
+    },
+
+  ];
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  const onChange = (e) => {
+    setValues({ ...values, [e.target.name]: e.target.value });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <form onSubmit={handleSubmit}>
+
+        <h1>Log in</h1>
+        {initialState.map((input) => (<FormInput key={input.id}{...input} value={values[input.name]} onChange={onChange} />
+        ))}
+        <button>Submit</button>
+      </form>
     </div>
   );
-}
+};
 
 export default App;
