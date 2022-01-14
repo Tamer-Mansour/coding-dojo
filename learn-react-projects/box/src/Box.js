ecdd1ca15f7eec532cbe78678d8b51aaeb6febcc
Tamer <tmansour720@gmail.com>
2022-01-14 13:21:15 +0200

    with-just-one-adddedd

--- learn-react-projects/box/src/Box.js
+++ learn-react-projects/box/src/Box.js
@@ -4,7 +4,8 @@ class Box extends Component {
   render() {
     return (
       <div>
-        <div style={{width: "100px",height: "100px",backgroundColor: `${this.props.color}`}}/>
+        <div style={{ width: `${this.props.width}px`, height: `${this.props.height}px`, backgroundColor: `${this.props.color}` }} />
+        <button onClick={() => this.props.removeBox(this.props.id)}>X</button>
       </div>
     );
   }
--- learn-react-projects/box/src/NewBox.js
+++ learn-react-projects/box/src/NewBox.js
@@ -3,26 +3,35 @@ class NewBoxForm extends Component {
 
   constructor(props) {
     super(props);
-    this.state = { color: "" }
+    this.state = { height: "", width: "", color: "" }
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleChange(evt) {
     this.setState({
-      [evt.target.name]: evt.target.value});
+      [evt.target.name]: evt.target.value
+    });
   }
 
   handleSubmit(evt) {
     evt.preventDefault();
     const newBox = { ...this.state }
     this.props.createBox(newBox);
-    this.setState({ color: "" })
+    this.setState({ height: "", width: "", color: "" })
   }
 
   render() {
     return (
       <form onSubmit={this.handleSubmit} >
+        <div>
+          <label htmlFor="width: ">width</label>
+          <input type="number" name="width" onChange={this.handleChange} value={this.state.width} />
+        </div>
+        <div>
+          <label htmlFor="height: ">height</label>
+          <input type="number" name="height" onChange={this.handleChange} value={this.state.height} />
+        </div>
         <div>
           <label htmlFor="color">Color</label>
           <input type="text" name="color" onChange={this.handleChange} value={this.state.color} />
