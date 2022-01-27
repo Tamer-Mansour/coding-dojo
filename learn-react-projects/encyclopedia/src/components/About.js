26f664c54ea477f1c66fd0c307fbe896424faa9f
Tamer Mansour <tmansour720@gmail.com>
2022-01-27 19:56:41 +0200

    third-update-from-my-project

--- learn-react-projects/encyclopedia/src/components/About.js
+++ learn-react-projects/encyclopedia/src/components/About.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import "./About.css";
 export const About = (props) => {
-    return <h1 style={{ color: "blue" }}>About Component</h1>;
+    return <h1 className='welcome'>About Component</h1>;
 };
--- learn-react-projects/encyclopedia/src/components/Home.js
+++ learn-react-projects/encyclopedia/src/components/Home.js
@@ -1,47 +1,31 @@
 import React from "react";
 import "./Home.css"
 import Task from "../Images/Task.png"
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
+import ToDoList from "../ToDo/ToDoList";
+
 const Home = (props) => {
-    return ( <
-        >
-        <
-        div >
-
-        <
-        /div> <
-        div className = "card sort"
-        style = {
-            { width: "18rem" } } >
-        <
-        img className = "card-img-top"
-        src = { Task }
-        alt = "task" / >
-        <
-        div className = "card-body" >
-        <
-        h5 className = "card-title" > Card title < /h5> <
-        p className = "card-text" > Some quick example text to build on the card title and make up the bulk of the card 's content.</p> <
-        button className = "btn btn-primary" > Go somewhere < /button> <
-        /div> <
-        /div> <
-        div className = "card sort"
-        style = {
-            { width: "18rem" } } >
-        <
-        img className = "card-img-top"
-        src = { Task }
-        alt = "task" / >
-        <
-        div className = "card-body" >
-        <
-        h5 className = "card-title" > Card title < /h5> <
-        p className = "card-text" > Some quick example text to build on the card title and make up the bulk of the card 's content.</p> <
-        button className = "btn btn-primary" > Go somewhere < /button> <
-        /div> <
-        /div>
-
-        <
-        />
+    return (
+        <>
+            <div>
+                <h1 className="text-center">Welcome to my web site</h1>
+            </div>
+            <div className="card sort" style={{ width: "18rem" }}>
+                <img className="card-img-top" src={Task} alt="task" />
+                <div className="card-body">
+                    <h5 className="card-title">To Do list</h5>
+                    <p className="card-text">I made this task when i learn react coding dojo.</p>
+                    <button className="btn btn-primary"><Link to="/todo" style={{ "color": "white" }}>Go to task</Link></button>
+                </div>
+                <BrowserRouter>
+                    <Switch>
+                        <Route path="/todo">
+                            <ToDoList />
+                        </Route>
+                    </Switch>
+                </BrowserRouter>
+            </div>
+        </>
     );
 };
 export default Home;
