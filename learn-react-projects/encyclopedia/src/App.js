9715567653e67789088c4ce27ee97295ad6a0684
Tamer <tmansour720@gmail.com>
2022-01-27 16:24:24 +0200

    to-resume-from-other-pc

--- learn-react-projects/encyclopedia/src/App.js
+++ learn-react-projects/encyclopedia/src/App.js
@@ -1,10 +1,30 @@
-import './App.css';
+import React from "react";
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
+
+const Home = (props) => {
+  return <h1 style={{ color: "red" }}>Home Component</h1>;
+};
+
+const About = (props) => {
+  return <h1 style={{ color: "blue" }}>About Component</h1>;
+};
 
 function App() {
   return (
-    <div className="App">
- 
-    </div>
+    <BrowserRouter>
+      <h1>Routing Example</h1>
+      <p>
+        <Link to="/">Home</Link>|<Link to="/about">About</Link>
+      </p>
+      <Switch>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route exact path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
