8a1e37a713ad59e93677d44d9d0cb5dcb1f7e8c6
Tamer Mansour <78074218+Tamer-Mansour@users.noreply.github.com>
2022-02-08 15:26:48 +0200

    project-full-stack-node-mongodb-stoped

---
+++ React Full-Stack Site/Front-End/src/NavBar.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
+import ArticlesListPage from "./pages/ArticlesListPage";
+import ArticlePage from "./pages/ArticlePage";
+import NotFoundPage from "./pages/NotFoundPage";
+const NavBar = () => {
+  return (
+    <>
+      <BrowserRouter>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/articles-list">Articles</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <div id="page-body">
+          <Switch>
+            <Route path="/" component={HomePage} exact />
+            <Route path="/about" component={AboutPage} />
+            <Route path="/articles-list" component={ArticlesListPage} />
+            <Route path="/article/:name" component={ArticlePage} />
+            <Route path="/*" component={NotFoundPage} />
+          </Switch>
+        </div>
+      </BrowserRouter>
+    </>
+  );
+};
+
+export default NavBar;
